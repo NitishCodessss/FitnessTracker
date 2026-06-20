87fe0a7980ffff2e7f8c14569b6c3b767a48545e
NitishCodessss <nitish304844@gmail.com>
2026-06-20 17:00:20 +0530

    add the pages and define routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
 
+import { Routes, Route } from 'react-router'
 import './index.css'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import FoodLog from './pages/FoodLog'
+import ActivityLog from './pages/ActivityLog'
+import Profile from './pages/Profile'
+
+
 
 function App() {
 
 
   return (
     <>
-    
+      <Routes>
+        <Route path='/' element={<Layout />}>
+            <Route index element={<Dashboard />} />
+            <Route path='food' element={<FoodLog />} />
+            <Route path='activity' element={<ActivityLog />} />
+            <Route path='profile' element={<Profile/>} />
+        </Route>
+      </Routes>
     </>
   )
 }
